c79d350147bb07cf72ed3ea32d71179452bf0859
NikolasBudzel <nikobudzek2006@gmail.com>
2024-09-28 20:45:10 +0200

    d.u 28.9.

---
+++ ulohy 1-14.py
@@ -0,0 +1,26 @@
+# #uloha 1
+# print("uloha 1:")
+# for i in range (1,11):
+#     print(i)
+#
+# #uloha 2 a
+# print("uloha 2 a:")
+# for i in range(1,int(input("N: "))+1):
+#     print(i)
+
+#uloha 2 b
+# print("uloha 2 b:")
+# N = int(input("N: "))+1
+# for i in range(1,N):
+#     if i == N-1:
+#         print(i)
+#     else:
+#         print(i, end=",")
+
+#uloha 3
+# print("uloha 3:")
+# N = int(input("N: "))+1
+# for i in range(5,N,2):
+#     print(i)
+
+
