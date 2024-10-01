e55440fd99a272da3ce7158c67afcbc9e78b8ea5
NikolasBudzel <nikobudzek2006@gmail.com>
2024-10-01 08:45:30 +0200

    sucet cifier, cislo odzadu

---
+++ sucet cifier.py
@@ -0,0 +1,21 @@
+# def sucet_cifier(cislo):
+#     cifra = 0
+#     while cislo > 0:
+#         ld = cislo % 10
+#         #spocitanie parnych
+#         if ld % 2 == 0:
+#             cifra += ld
+#         cislo = cislo // 10
+#     return cifra
+#
+# print(sucet_cifier(125))
+
+def cislo_odzadu(cislo):
+    cifra = 0 # inicializovali sme  premennu s hodnotou nula
+    while cislo > 0:
+        ld = cislo % 10
+        cifra = cifra*10 + ld # cislo odzadu
+        cislo = cislo // 10
+    return cifra
+
+print(sucet_cifier(125))
