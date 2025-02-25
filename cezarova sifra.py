30c1114b153b0bfbdd060aa4b98267df7592838d
NikolasBudzel <nikobudzek2006@gmail.com>
2025-02-25 09:25:43 +0100

    25.2.

--- cezarova sifra.py
+++ cezarova sifra.py
@@ -1,23 +1,59 @@
-def cezarova_sifra(input:str,offset:int):
-    output = ""
-    for char in input:
-        if(97<=ord(char) and ord(char)<=122):
-            output += chr((ord(char) - 97 + offset) % 26 + 97)
-    return output
+# def cezarova_sifra(input:str,offset:int):
+#     output = ""
+#     for char in input:
+#         if(97<=ord(char) and ord(char)<=122):
+#             output += chr((ord(char) - 97 + offset) % 26 + 97)
+#     return output
+#
+# def desifruj(input:str,offset:int):
+#     output = ("")
+#     for char in input:
+#         if (97 <= ord(char) and ord(char) <= 122):
+#             output += chr((ord(char) - 97 - offset) % 26 + 97)
+#     return output
+#
+# def predlz_kluc(retazec,kluc):
+#     kolko = len(retazec)//len(kluc)
+#     zvysok = len(retazec)%len(kluc)
+#     return kluc*kolko+kluc[:zvysok:]
+#
+# def sifra ():
+#     kluc = input("zadaj kluc: ")
+#     retazec = input("zadaj rezatec: ")
+#
+#     ret1 = predlz_kluc(retazec,kluc)
+#
 
-def desifruj(input:str,offset:int):
-    output = ("")
-    for char in input:
-        if (97 <= ord(char) and ord(char) <= 122):
-            output += chr((ord(char) - 97 - offset) % 26 + 97)
-    return output
+
+def cezarova_sifra_retazec(text:str,key:str):
+    result = ""
+    for i, char in enumerate(text):
+        if char.isascii():
+            if (ord(char) >= ord("a") and ord(char) <= ord("z")):
+                is_upper = char.isupper()
+                is_space = char == " "
+                char = char.lower()
+                shift = ord(key[i % len(key)].lower()) - ord('a')
+                shifted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+                if is_upper:
+                    shifted_char = shifted_char.upper()
+                    result += shifted_char
+            else:
+                result += char
+        else:
+            result += char
+    return result
 
 def predlz_kluc(retazec,kluc):
     kolko = len(retazec)//len(kluc)
     zvysok = len(retazec)%len(kluc)
     return kluc*kolko+kluc[:zvysok:]
-def sifra ():
-    kluc = input("zadaj kluc: ")
+
+def cezar_retazec ():
     retazec = input("zadaj rezatec: ")
-    ret1 = predlz_kluc(retazec,kluc)
-def
+    kluc = input("zadaj kluc: ")
+    predlzenz_kluc = predlz_kluc(retazec, kluc)
+    zasifrovany_text = cezarova_sifra_retazec(retazec, predlzenz_kluc)
+    return zasifrovany_text
+
+print(cezar_retazec())
